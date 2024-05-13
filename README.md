46003294b1e18fd80f84ebb4220d2efbae796c99
andreortiz82 <andreortiz82@gmail.com>
2024-05-12 19:59:07 -0500

    updating project to use antd and ao-ui

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ![img](./public/cover.png)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This template provides a minimal setup to get React working in Vite and some ESLint rules.
+
 Includes:
 
 - React / Typescript
@@ -10,7 +11,8 @@ Includes:
 - CSS Modules
 - [clsx](https://github.com/lukeed/clsx)
 - [Design Tokens](https://github.com/andreortiz82/ao-design-tokens)
-- [React UI Lib](https://github.com/andreortiz82/ao-ui-lib)
+- [Ant Design 5](https://ant.design/components/overview/)
+- [AO-UI-Lib](https://github.com/andreortiz82/ao-ui-lib)
 
 ## Get Started
 
@@ -22,25 +24,3 @@ npm run dev
 ```
 
 The project runs on [localhost:3000](http://localhost:3000/)
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
-
-- Configure the top-level `parserOptions` property like this:
-
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["./tsconfig.json", "./tsconfig.node.json"],
-    tsconfigRootDir: __dirname,
-  },
-};
-```
-
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,35 @@ import { useState } from "react";
 import clsx from "clsx";
 import style from "./App.module.css";
 import logo from "./assets/react.svg";
-import { Button } from "ao-ui-lib";
+
+import { Button } from "antd";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className={clsx(style["card"])}>
-        <div className={style["logo"]}>
-          <img src={logo} alt="logo" />
-        </div>
-        <h1>React + Vite Template</h1>
-        <p>
-          This project also uses{" "}
-          <a href="https://github.com/andreortiz82/ao-design-tokens">
-            AO Design Tokens
-          </a>{" "}
-          and <a href="https://github.com/andreortiz82/ao-ui-lib">AO UI Lib</a>
-        </p>
-        <p>
-          👋 Made by hand. <a href="http://andreortiz.com">AO</a>
-        </p>
-        <div>
-          <Button
-            onClick={() => setCount((count) => count + 1)}
-            label={`count is ${count}`}
-          />
-        </div>
+    <div className={clsx(style["card"])}>
+      <div className={style["logo"]}>
+        <img src={logo} alt="logo" />
       </div>
-    </>
+      <h1>React + Vite Template</h1>
+      <p>
+        This project also uses{" "}
+        <a href="https://github.com/andreortiz82/ao-design-tokens">
+          AO Design Tokens
+        </a>{" "}
+        and <a href="https://github.com/andreortiz82/ao-ui-lib">AO UI Lib</a>
+      </p>
+      <p>
+        👋 Made by hand. <a href="http://andreortiz.com">AO</a>
+      </p>
+      <div>
+        <Button
+          type="primary"
+          onClick={() => setCount((count) => count + 1)}
+        >{`count is ${count}`}</Button>
+      </div>
+    </div>
   );
 }
 
